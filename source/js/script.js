5846fd9ad9dd118470ceb022e8fc896cbfbe5c3e
alexey <sunnyplus@mail.ru>
2019-12-26 23:50:59 +0300

    form mobile completed

--- source/js/script.js
+++ source/js/script.js
@@ -2,19 +2,35 @@ var closeBusinessBtn = document.querySelector(".tarifs-business__close"),
     tarifBusiness = document.querySelector(".tarifs-business"),
     showBusinessBtn = document.querySelector(".tarifs__show-business"),
     mainnavBtn = document.querySelector(".main-nav__button"),
-    mainnavContent = document.querySelector(".main-nav__content");
+    mainnavContent = document.querySelector(".main-nav__content"),
+    countryChoose = document.querySelector("#country-3"),
+    countryCloseBtn = document.querySelector(".country-select--choose .country-select__button-delete"),
+    countryBtnWrapper = document.querySelector(".country-select--choose"),
+    countryDropdown = document.querySelector(".step__countries-wrapper");
 
-showBusinessBtn.addEventListener('click', function(evt) {
+if(showBusinessBtn) {
+  showBusinessBtn.addEventListener('click', function(evt) {
   evt.preventDefault();
   tarifBusiness.classList.add("tarifs-business--opened");
-});
+  });
+}
 
-closeBusinessBtn.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  tarifBusiness.classList.remove("tarifs-business--opened");
-});
+if(closeBusinessBtn) {
+  closeBusinessBtn.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    tarifBusiness.classList.remove("tarifs-business--opened");
+  });
+}
 
 mainnavBtn.addEventListener('click', function(evt) {
   evt.preventDefault();
   mainnavContent.classList.toggle("main-nav__content--closed");
+});
+
+countryChoose.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  countryCloseBtn.classList.toggle("country-select__button-delete--closed");
+  countryChoose.classList.toggle("country-select__input--darkblue");
+  countryBtnWrapper.classList.toggle("country-select--opened");
+  countryDropdown.classList.toggle("step__countries-wrapper--opened");
 });
